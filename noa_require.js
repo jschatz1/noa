e404354d9113db09d9fcaa65488b24e27c8d4a08
Jacob Schatz <jschatz1@gmail.com>
2020-11-14 23:11:36 -0500

    up

--- noa_require.js
+++ noa_require.js
@@ -1,5 +1,5 @@
 const NoaParser = require('./out.js');
-let value = NoaParser.parse('c = "z";', {
+let value = NoaParser.parse('7 + 2;', {
   captureLocations: true
 });
 console.log(JSON.stringify(value, null, 2));
